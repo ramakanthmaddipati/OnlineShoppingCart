8123155a12c529de918f9ce370cf67e1b362ab81
ramakanthmaddipati <58617514+ramakanthmaddipati@users.noreply.github.com>
2021-02-03 18:52:45 +0530

    Add files via upload

---
+++ OnlineShoppingExceptionLib/OnlineShoppingException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingExceptionLib
+{
+    /// <summary>
+    /// This Class is used for Custom Exception by user
+    /// </summary>
+    public class OnlineShoppingException : Exception
+    {
+        //Inheriting the basic Exception class
+        public OnlineShoppingException(string errMsg) : base(errMsg)
+        {
+            //TO Do
+        }
+    }
+}
